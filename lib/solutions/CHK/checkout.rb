4c22631612cbb324b1ce684a9cbfe5f3b7a59241
sbx_user1051 <sbx_user1051@169.254.72.157>
2019-09-19 13:17:42 +0000

    Thu Sep 19 13:17:42 UTC 2019

--- lib/solutions/CHK/checkout.rb
+++ lib/solutions/CHK/checkout.rb
@@ -28,11 +28,8 @@ class Checkout
       each_sku_count[sku] = each_sku_count[sku] + 1
     end
 
-    best_solution = Checkout.traverse_discounts(0, each_sku_count).min_by do |total, remaining_basket|
-      total + Checkout.basic_price(remaining_basket)
-    end
+    Checkout.traverse_discounts(0, each_sku_count).map(&:first).min
 
-    best_solution
   end
 
   def self.valid_sku?(sku)
@@ -45,14 +42,6 @@ class Checkout
 
   def self.discount(sku)
     DISCOUNTS[sku]
-  end
-
-  def self.basic_price(basket)
-    total = 0
-    basket.each_pair do |sku, count|
-      total = total + (PRICES[sku] * count)
-    end
-    total
   end
 
   def self.apply_discount(discount, basket)
@@ -90,7 +79,7 @@ class Checkout
       if basket.empty? then
         [[total, basket]]
       else
-        [[total, basket]] + self.traverse_discounts(total, basket)
+        self.traverse_discounts(total, basket)
       end
     end
 
@@ -99,6 +88,7 @@ class Checkout
   end
 
 end
+
 
 
 
