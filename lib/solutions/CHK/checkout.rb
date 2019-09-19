8be475df73a4497c67c430887bfdff1146f7abf0
sbx_user1051 <sbx_user1051@169.254.72.157>
2019-09-19 12:23:42 +0000

    Thu Sep 19 12:23:42 UTC 2019

--- lib/solutions/CHK/checkout.rb
+++ lib/solutions/CHK/checkout.rb
@@ -47,23 +47,30 @@ class Checkout
     DISCOUNTS[sku]
   end
 
+
   def self.discount_basket(basket)
     discounted_total = 0
     leftovers = {}
-    basket.each do |sku, count|
-      if discount = DISCOUNTS[sku] then
-        num_for_discount, discount_total = discount
-        leftovers[sku] = count.modulo num_for_discount
-        discounted_total = discounted_total + (count.div(num_for_discount) * discount_total)
-      else
-        leftovers[sku] = count
-      end
+
+    DISCOUNTS.map do |prods, val|
+      puts [prods, val].inspect
     end
+    # basket.each do |sku, count|
+
+    #   if discount = DISCOUNTS[sku] then
+    #     num_for_discount, discount_total = discount
+    #     leftovers[sku] = count.modulo num_for_discount
+    #     discounted_total = discounted_total + (count.div(num_for_discount) * discount_total)
+    #   else
+    #     leftovers[sku] = count
+    #   end
+    # end
     [discounted_total, leftovers]
   end
 
 
 end
+
 
 
 
