98e7b946c0bd7b9165751c8d4dd1992c1ce10819
sbx_user1051 <sbx_user1051@169.254.72.157>
2019-09-19 11:21:30 +0000

    Thu Sep 19 11:21:30 UTC 2019

--- lib/solutions/CHK/checkout.rb
+++ lib/solutions/CHK/checkout.rb
@@ -3,8 +3,24 @@ class Checkout
 
   def checkout(skus)
     return -1 unless skus.is_a? String
+    return -1 unless skus.each_char.all? &valid_sku?
     return 0
   end
 
 end
+
+def valid_sku?(sku)
+  case sku
+    when "A"
+      true
+    when "B"
+      true
+    when "C"
+      true
+    when "D"
+      true
+  end
+  false
+end
+
 
