f4717f274b6a14925903b5ab844bc5be57804d0d
sbx_user1051 <sbx_user1051@169.254.72.157>
2019-09-19 11:24:30 +0000

    Thu Sep 19 11:24:30 UTC 2019

--- lib/solutions/CHK/checkout.rb
+++ lib/solutions/CHK/checkout.rb
@@ -3,24 +3,25 @@ class Checkout
 
   def checkout(skus)
     return -1 unless skus.is_a? String
-    return -1 unless skus.each_char.all? &valid_sku?
+    return -1 unless skus.each_char.all? {|sku| valid_sku? sku }
     return 0
   end
 
+  def self.valid_sku?(sku)
+    case sku
+      when "A"
+        true
+      when "B"
+        true
+      when "C"
+        true
+      when "D"
+        true
+    end
+    false
+  end
+
 end
 
-def valid_sku?(sku)
-  case sku
-    when "A"
-      true
-    when "B"
-      true
-    when "C"
-      true
-    when "D"
-      true
-  end
-  false
-end
 
 
--- test/CHK/chk_test.rb
+++ test/CHK/chk_test.rb
@@ -11,14 +11,22 @@ class ClientTest < Minitest::Test
 
   def test_chk
     assert_equal 0, Checkout.new.checkout(""), "Empty basket should cost nothing"
+    assert_equal
 
     assert_equal -1, Checkout.new.checkout("ABCDE"), "Basket with non-existent products should return -1"
     assert_equal -1, Checkout.new.checkout(100), "Non-string basket should return -1"
   end
 
+  def test_valid_sku?
+    assert_equal true, Checkout.valid_sku?("A"), "A is a valid sku"
+    assert_equal false, Checkout.valid_sku?("Q"), "Q is not a valid sku"
+  end
+
   def test_prices
+    assert_equal 0
 
   end
 
 end
+
 
